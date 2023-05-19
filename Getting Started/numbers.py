a7ef4566c9a6a2d5e3ab9e597b9af249912dc0fd
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-05-19 15:07:03 -0700

    BODMAS on numbers

--- Getting Started/numbers.py
+++ Getting Started/numbers.py
@@ -1,3 +1,7 @@
+# BODMAS property
+print(2 * 5 - 1); # 9
+print(2 * (5 - 1)); # 8
+"""
 print(100 + 10);# 100
 print(100 - 10); # 90
 print(100 / 10); # 10.0
@@ -9,3 +13,4 @@ print(7.5 % 5); # 2.5
 
 print(type(2)); # <class 'int'>
 print(type(100 / 10)); # <class 'float'>
+"""

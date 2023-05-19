95732bddc516d890a9bae23c64c2bb11cd67047c
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-05-19 15:03:22 -0700

    Numbers and basic operations

---
+++ Getting Started/numbers.py
@@ -0,0 +1,11 @@
+print(100 + 10);# 100
+print(100 - 10); # 90
+print(100 / 10); # 10.0
+print(2 / 4); # 0.5
+print(100 * 10); # 1000
+print(5 % 3); # 2
+print(10 % 2); # 0
+print(7.5 % 5); # 2.5
+
+print(type(2)); # <class 'int'>
+print(type(100 / 10)); # <class 'float'>

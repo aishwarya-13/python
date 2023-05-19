2f8e5170a21a5ec12dd83e73b0eb7eaca6cca941
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-05-19 14:08:12 -0700

    Variables in python

---
+++ Getting Started/variables.py
@@ -0,0 +1,11 @@
+number = 1
+print(number) # prints 1
+# print(NUMBER) # NUMBER is not defined error
+NUMBER = 100
+print(NUMBER) #prints 100
+
+#Python is dynamically typed
+print(type(number)) # <class 'int'>
+number = 'hello';
+print(type(number)) # <class 'str'>
+print(number); # prints hello

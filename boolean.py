c4f4dd293a568941f13652f76315c82ed150b464
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-05-20 11:32:17 -0700

    Boolean

---
+++ boolean.py
@@ -0,0 +1,9 @@
+"""
+-> Boolean values in Python are capitalized (first letter is capital)
+-> False / True -> valid
+-> False / true -> invalid
+"""
+a = True
+b = False
+#c = true #NameError: name 'true' is not defined. Did you mean: 'True'?
+print(type(a)) # <class 'bool'>

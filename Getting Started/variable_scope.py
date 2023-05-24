8689292f46eb2c54cee0715b152b86e1b575426d
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-05-23 23:12:27 -0700

    Changing value of global var in function

--- Getting Started/variable_scope.py
+++ Getting Started/variable_scope.py
@@ -2,8 +2,9 @@
 Two types of scope in python
 1] Global
 2] Local scope: functions create local scopes inside them
-  Local variable takes priority over global variables
-  Local variables are destroyed after function is done executing
+->Local variable takes priority over global variables
+->Local variables are destroyed after function is done executing
+->Python does not let you change the value of global variable. So what it does is, it creates a local variable
 """
 
 """
@@ -14,13 +15,13 @@ d = 2 #global variable
 
 def fun1():
     c=1 #local variable
-    d=20 #local variable with same name as global variable d
+    d=20 #trying to change the value of global variable but Python does not let you change the value of global variable
     print(a)
     print(c)
     print(d)
 
 def fun2():
-    d = 100 #local variable with same name as global variable d
+    d = 100 #trying to change the value of global variable but Python does not let you change the value of global variable
     print(a)
     #print(c) #NameError: name 'c' is not defined
     print(d)
@@ -38,3 +39,20 @@ def fun3():
     print(a)
 
 fun3()
+
+"""
+How to change global variable value in function
+"""
+val = 1000
+
+def fun4():
+    global val
+    val = 1
+    print('Changed value of global variable::::', val)
+
+def fun5():
+    print('value of global in fun5()::::',val)
+
+print('Global variable::::', val)
+fun4()
+fun5()

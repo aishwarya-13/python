b9393631777cc388701fd8adaa8b61cff3be4e6d
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-05-24 16:43:14 -0700

    packing and unpacking arguments

--- Getting Started/arguments_and_parameters.py
+++ Getting Started/arguments_and_parameters.py
@@ -7,3 +7,46 @@ def about(name, age, likes = 'Python'): #These are function parameters
 print(about('Jack', 30, 'Javscript')) #These are function arguments
 print(about(age = 40, name = 'Jill', likes = 'Java')) #These are function arguments
 print(about('Jeanine', 20))
+
+"""
+Packing and unpacking arguments
+"""
+
+#unpacking
+mylist = [1,2,3,4,5]
+print(mylist) #[1, 2, 3, 4, 5]
+print(*mylist) #1 2 3 4 5
+
+a = 'hello'
+print(a) #hello
+print(*a) #h e l l o
+
+#Packing
+def add(*numbers):
+    total = 0
+    for num in numbers:
+        total+= num
+    return total
+
+print(add(5, 10))
+print(add(1,2,3,4,5,10))
+
+"""
+Unpack Dictionaries
+"""
+def about(name, age, likes):
+    return "Meet {}! They are {} old and likes {}".format(name, age, likes)
+
+dicti = {
+    "age": 30,
+    "name": "Aishwarya",
+    "likes":"Python"
+}
+print(about(**dicti))
+
+def foo(**kwargs):
+    for key, value in kwargs.items():
+        print("{}:{}".format(key, value))
+
+foo(aish = "Female", rohit = "Male") #aish:Female rohit:Male
+foo(aish = "Female", rohit = "Male", ojas = "Male") #aish:Female rohit:Male ojas:Male
